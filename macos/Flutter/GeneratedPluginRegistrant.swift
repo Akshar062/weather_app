c2162840fb61acd705e35ea8972f4ba03a0e50eb
Akshar Kalathiya <147808278+Akshar062@users.noreply.github.com>
2023-11-23 16:45:40 +0530

    minor change

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import location
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
 }
